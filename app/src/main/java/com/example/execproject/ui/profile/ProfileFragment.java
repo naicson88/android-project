e96c8f24ffaee38ee9297ad1d0fdc042e1ea35e8
a91628319 <naicson88@hotmail.com>
2022-04-16 00:37:29 -0300

    save info

--- app/src/main/java/com/example/execproject/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/execproject/ui/profile/ProfileFragment.java
@@ -2,6 +2,7 @@ package com.example.execproject.ui.profile;
 
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -28,6 +30,12 @@ import com.example.execproject.databinding.FragmentHomeBinding;
 import com.example.execproject.databinding.FragmentProfileBinding;
 import com.example.execproject.ui.home.HomeViewModel;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -35,10 +43,14 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
 
     private FragmentProfileBinding binding;
     private DatePickerDialog datePickerDialog;
-    private Button dateButton;
+    private Button dateButton, submitButton;
     private RadioGroup radioGroup;
-    private RadioButton radioButtonClass;
+    private RadioButton radioButton;
     private View viewAux;
+
+    EditText userNameInput, userHeightInput, userWeightInput;
+
+    String  userName, userHeight, userWeight, userGender, userBirthDate;
 
     public ProfileFragment(){
 
@@ -64,11 +76,124 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-      //  ImageView imageView = (ImageView) getView().findViewById(R.id.foo);
         viewAux = getView();
         onRadioButtonClicked();
-        // or  (ImageView) view.findViewById(R.id.foo);
+
+        userNameInput = (EditText) view.findViewById(R.id.names);
+        userHeightInput = (EditText) view.findViewById(R.id.inputHeight);
+        userWeightInput = (EditText) view.findViewById(R.id.inputWeight);
+        int selectedId = radioGroup.getCheckedRadioButtonId();
+        radioButton =  (RadioButton) view.findViewById(selectedId);
+        dateButton =  (Button) view.findViewById(R.id.datePickerButton);
+        submitButton = (Button) viewAux.findViewById(R.id.submitButtonProfile);
+
+        initSubmitButton();
+
+        readInformationsSaved();
+
     }
+
+    private void initSubmitButton(){
+
+        submitButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                userName = userNameInput.getText().toString();
+                userHeight = userHeightInput.getText().toString();
+                userWeight = userWeightInput.getText().toString();
+                userGender = radioButton.getText().toString();
+                userBirthDate = dateButton.getText().toString();
+
+                //Toast.makeText(getContext(), userName + " " + userHeight + " " + userWeight + " " + userGender +" " + userBirthDate, Toast.LENGTH_SHORT).show();
+
+               Boolean isFormValid =  isValidProfileInformations(userName,  userHeight,  userWeight,  userGender,  userBirthDate);
+
+               if(!isFormValid)
+                   return;
+
+                saveInformations(userName,  userHeight,  userWeight,  userGender,  userBirthDate);
+
+            }
+        });
+    }
+
+    private Boolean isValidProfileInformations(String userName, String userHeight, String userWeight, String userGender, String userBirthDate){
+
+           try{
+
+               Float heightFloat = Float.valueOf(userHeight);
+               Float weightFloat =  Float.valueOf(userWeight);
+
+               if(userName == null || userName.isEmpty() || userName.length() < 2){
+                   Toast.makeText(getContext(), R.string.errorUserName, Toast.LENGTH_SHORT).show();
+                   return false;
+               }
+               if(heightFloat < 0.60 || heightFloat > 3.0){
+                   Toast.makeText(getContext(), R.string.errorUserHeight, Toast.LENGTH_SHORT).show();
+                   return false;
+               }
+
+               if(weightFloat < 20.0 || weightFloat > 500){
+                   Toast.makeText(getContext(), R.string.errorUserWeight, Toast.LENGTH_SHORT).show();
+                   return false;
+               }
+               if(userGender == null || userGender.isEmpty()){
+                   Toast.makeText(getContext(), R.string.errorGender, Toast.LENGTH_SHORT).show();
+                   return false;
+               }
+
+           } catch(Exception e){
+               e.printStackTrace();
+               Toast.makeText(getContext(), R.string.errorProfileExceptionValidations, Toast.LENGTH_SHORT).show();
+           }
+
+           return true;
+    }
+
+    public void saveInformations(String userName, String userHeight, String userWeight, String userGender, String userBirthDate){
+        try {
+
+            FileOutputStream fileOutputStream = getActivity().openFileOutput("Profile File.txt", Context.MODE_PRIVATE);
+            fileOutputStream.write(userName.getBytes());
+            fileOutputStream.write(userHeight.getBytes());
+            fileOutputStream.write(userWeight.getBytes());
+            fileOutputStream.write(userGender.getBytes());
+            fileOutputStream.write(userBirthDate.getBytes());
+
+            fileOutputStream.close();
+
+            Toast.makeText(getContext(), R.string.savedMessage, Toast.LENGTH_SHORT).show();
+
+        }catch(FileNotFoundException e){
+            e.printStackTrace();
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void readInformationsSaved(){
+        try{
+
+            FileInputStream fileInputStream = getActivity().openFileInput("Profile File.txt");
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+
+            BufferedReader bufferedReader = new BufferedReader((inputStreamReader));
+            StringBuffer stringBuffer = new StringBuffer();
+
+            String line;
+            while((line = bufferedReader.readLine()) != null){
+                stringBuffer.append(line + "\n");
+            }
+            Toast.makeText(getContext(), stringBuffer.toString(), Toast.LENGTH_SHORT).show();
+
+
+        }catch(FileNotFoundException e){
+            e.printStackTrace();
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
     private void initDatePicker() {
         DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
             @Override
@@ -120,22 +245,6 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
         }
     }
 
-//    public void onRadioButtonClicked(View view) {
-//        int radioId = radioGroup.getCheckedRadioButtonId();
-//        String resp = "";
-//
-//        switch (view.getId()){
-//            case R.id.radioButtonMale:
-//                resp = "Male";
-//                break;
-//            case R.id.radioButtonFemale:
-//                resp = "Female";
-//                break;
-//        }
-//
-//        Toast.makeText(getContext(), resp, Toast.LENGTH_SHORT).show();
-//    }
-
 
     public void onRadioButtonClicked (){
 
@@ -143,10 +252,11 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
-                radioButtonClass = viewAux.findViewById(checkedId);
-                String resp = radioButtonClass.getText().toString();
+                radioButton = viewAux.findViewById(checkedId);
+                String resp = radioButton.getText().toString();
                 Toast.makeText(getContext(), resp, Toast.LENGTH_SHORT).show();
             }
         });
     }
+
 }
