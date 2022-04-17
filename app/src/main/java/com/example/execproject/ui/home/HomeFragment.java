5bb9eecd9040d7fc8de044e715f00b2d22f332cf
a91628319 <naicson88@hotmail.com>
2022-04-16 22:01:30 -0300

    home

--- app/src/main/java/com/example/execproject/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/execproject/ui/home/HomeFragment.java
@@ -4,17 +4,28 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.models.SlideModel;
+import com.example.execproject.R;
 import com.example.execproject.databinding.FragmentHomeBinding;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private View viewAux;
+    private ImageSlider imageSlider;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -25,9 +36,22 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+       // homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        viewAux = getView();
+
+        imageSlider = view.findViewById(R.id.sliderHome);
+
+        ArrayList<SlideModel> images = new ArrayList<>();
+        images.add(new SlideModel(R.drawable.walking, "Walking",null));
+        images.add(new SlideModel(R.drawable.running, "Running",  null));
+        images.add(new SlideModel(R.drawable.biking, "Biking",null));
+
+        imageSlider.setImageList(images);
     }
 
     @Override
